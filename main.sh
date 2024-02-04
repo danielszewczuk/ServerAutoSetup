fd8dc1415ecc73fb7a7538a7cd54172e6bc8cdcb
Daniel Szewczuk <danielszewczuk@users.noreply.github.com>
2024-02-04 20:16:22 +0100

    Automatic updates setup

--- main.sh
+++ main.sh
@@ -5,7 +5,15 @@ sed -i '/nameserver/d' /etc/resolv.conf
 sudo timedatectl set-timezone Europe/Warsaw
 apt update -y
 apt upgrade -y
-apt install git curl python3 pip -y
+apt install git unattended-upgrades python3 pip -y
+cat << EOF > /etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Download-Upgradeable-Packages "1";
+APT::Periodic::AutocleanInterval "7";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+unattended-upgrades --dry-run --debug
 rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 python3 -m pip install --upgrade pip
 pip install py-cord
+curl -s https://binaries.twingate.com/client/linux/install.sh | sudo bash
