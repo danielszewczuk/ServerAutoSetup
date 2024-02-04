2cc9b1fe6e33ff9f584b33d217c7c10349905c2c
Daniel Szewczuk <danielszewczuk@users.noreply.github.com>
2024-02-04 19:09:31 +0100

    Add server setup scripts and update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# serverautosetup
+# ServerAutoSetup
+Scripts I use to automate setting up my server.
+
+Usage:
+``` sudo ./main.sh ```
---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+sed -i '/nameserver/d' /etc/resolv.conf
+sudo timedatectl set-timezone Europe/Warsaw
+apt update -y
+apt upgrade -y
+apt install git curl python3 pip -y
+python3 -m pip install --upgrade pip
+pip install py-cord
+
+
+
+
+
